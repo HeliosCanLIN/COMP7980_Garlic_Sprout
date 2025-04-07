2bdda5dc9e130cd5cd268dd70180553b2b3abcf6
StewieYuen <yzhch2015@gmail.com>
2025-04-07 22:05:19 +0800

    拆分接口 增加点赞接口

--- routes/users.js
+++ routes/users.js
@@ -53,46 +53,5 @@ router.post('/login/', async function (req, res) {
     }
 })
 
-router.post('/changePassword/', async function (req, res) {
-    const db = await connectToDB();
-    try {
-        console.log(req.body);
-        var username = req.body.username;
-        var password = req.body.password;
-        var id=req.body.id;
-        let result = await db.collection("garlic_user").findOne({ _id: new ObjectId(id) });
-        if (result != null) {
-           let updateResult= await db.collection("garlic_user").updateOne({_id:new ObjectId(id)}, {$set: {password: password}});
-           if(updateResult!=null){
-               res.json({updateResult});
-           }else{
-               res.status(404).json({message: "更新失败，请稍后重试"});
-           }
-        } else {
-            res.status(404).json({message: "未找到用户"});
-        }
-    } catch (err) {
-        res.status(400).json({message: err.message});
-    } finally {
-        await db.client.close();
-    }
-})
-
-router.post('/deleteAcc/', async function (req, res) {
-    const db = await connectToDB();
-    try {
-        var id=req.body.id;
-        console.log(req.body);
-        let updateResult= await db.collection("garlic_user").updateOne({_id:new ObjectId(id)}, {$set: {deleteFlag: 0}});
-        if(updateResult!=null){
-            res.json({updateResult});
-        }else{
-            res.status(404).json({message: "更新失败，请稍后重试"});
-        }
-    }catch (err){res.status(400).json({ message: err.message });}
-    finally {
-        await db.client.close();
-    }
-})
 
 module.exports = router;
