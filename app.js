086d5c5f2ab645ee5058c553c143b07b33d27e73
Can LIN <24459828@life.hkbu.edu.hk>
2025-04-08 16:30:56 +0800

    ai与热帖

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var userProtectedRouter = require('./routes/users-protected');
 var postsRouter = require('./routes/posts');
 var testRouter = require('./routes/test');
 var likeRouter = require('./routes/like');
+var aiRouter = require('./routes/ai');
 
 var app = express();
 
@@ -19,43 +20,46 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/users', userProtectedRouter);
-app.use('/test',testRouter);
+app.use('/test', testRouter);
 app.use('/posts', postsRouter);
-app.use('/like',likeRouter);
+app.use('/like', likeRouter);
+app.use('/ai', aiRouter);
 
 var jwt = require('jsonwebtoken');
 var passport = require('passport');
 var BearerStrategy = require('passport-http-bearer').Strategy;
 passport.use(new BearerStrategy(
     function (token, done) {
-      jwt.verify(token, process.env.TOKEN_SECRET, function (err, decoded) {
-        if (err) { return done(err); }
-        return done(null, decoded, { scope: "all" });
-      });
+        jwt.verify(token, process.env.TOKEN_SECRET, function (err, decoded) {
+            if (err) {
+                return done(err);
+            }
+            return done(null, decoded, {scope: "all"});
+        });
     }
 ));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 process.env.TOKEN_SECRET = '8964TAM'
